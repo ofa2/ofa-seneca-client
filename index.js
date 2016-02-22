90266c8c98848eb3f8156a9dd3cc969b52df82ca
GongXian Cao <gongxiancao@hotmail.com>
2016-02-22 15:45:05 +0800

    allow config mq

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+var _ = require('lodash');
+
 module.exports = function (done) {
   var self = this;
+  var senecaConnectionName = (self.config.seneca || {}).connection;
+  var senecaConnection = self.config.connections[senecaConnectionName];
+  if(senecaConnectionName && !senecaConnection) {
+    throw new Error('unknown seneca connection:' + senecaConnectionName);
+  }
+
   self.senecaClient = self.seneca
-    .use('seneca-amqp-transport')
-    .client({
+    .use(require('seneca-amqp-transport'))
+    .client(_.merge({
       type: 'amqp',
       pin: 'role:api'
-    });
+    }, senecaConnection));
   process.nextTick(done);
 };
